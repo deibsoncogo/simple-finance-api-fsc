eef1bd61a89837f4548c48305156471ef3c38eb7
Deibson Cogo <deibsoncogo@outlook.com>
2024-03-02 20:01:36 -0300

    chore: implementação e execução de teste da aplicação

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 import "dotenv/config.js"
+import express from "express"
+import { postgresHelper } from "./src/db/postgres/helper.js"
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", async (request, response) => {
+  const results = await postgresHelper.query("SELECT * FROM users;")
+  response.send(JSON.stringify(results))
+})
+
+app.post("/api/users", async (request, response) => {
+  console.log("request.body =>", request.body)
+  response.status(201).send("User created")
+})
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`),
+)
