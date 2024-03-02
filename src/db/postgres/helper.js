55fdcd3692a3b88f73bd1ef4d52f733db9e20565
Deibson Cogo <deibsoncogo@outlook.com>
2024-03-02 08:05:10 -0300

    chore: configuração do banco de dados

---
+++ src/db/postgres/helper.js
@@ -0,0 +1,22 @@
+import pg from "pg"
+
+const { Pool } = pg
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  port: process.env.POSTGRES_PORT,
+  host: process.env.POSTGRES_HOST,
+})
+
+export const postgresHelper = {
+  query: async (query, params) => {
+    const client = await pool.connect()
+    const { rows } = await client.query(query, params)
+
+    await client.release()
+
+    return rows
+  },
+}
