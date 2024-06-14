b00450d3644b2578ae58943b056ec898bf11f3e0
Deibson Cogo <deibsoncogo@outlook.com>
2024-06-13 23:12:25 -0300

    feat: criação do create transaction repository

---
+++ src/repositories/transaction/create-transaction.js
@@ -0,0 +1,19 @@
+import { postgresHelper } from "../../db/postgres/helper.js"
+
+export class CreateTransactionRepository {
+  async execute(createTransactionParams) {
+    const createdUser = await postgresHelper.query(
+      "INSERT INTO transactions (id, user_id, name, date, amount, type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        createTransactionParams.id,
+        createTransactionParams.user_id,
+        createTransactionParams.name,
+        createTransactionParams.date,
+        createTransactionParams.amount,
+        createTransactionParams.type,
+      ],
+    )
+
+    return createdUser[0]
+  }
+}
