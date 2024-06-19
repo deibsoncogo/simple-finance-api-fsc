598b648d865dfb2a0182039b0d041bfc3f308434
Deibson Cogo <deibsoncogo@outlook.com>
2024-06-18 21:34:16 -0300

    feat: criação da rota create transaction

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {
   makeGetUserByIdController,
   makeUpdateUserController,
 } from "./src/factories/controllers/user.js"
+import { makeCreateTransactionController } from "./src/factories/controllers/transaction.js"
 
 const app = express()
 
@@ -39,6 +40,14 @@ app.delete("/api/users/:userId", async (request, response) => {
   const deleteUserController = makeDeleteUserController()
 
   const { statusCode, body } = await deleteUserController.handle(request)
+
+  response.status(statusCode).send(body)
+})
+
+app.post("/api/transactions", async (request, response) => {
+  const createTransactionController = makeCreateTransactionController()
+
+  const { statusCode, body } = await createTransactionController.handle(request)
 
   response.status(statusCode).send(body)
 })
