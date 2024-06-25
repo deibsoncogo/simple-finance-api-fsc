b4991eabf3db22c0f21f559602e5663ef6736d63
Deibson Cogo <deibsoncogo@outlook.com>
2024-06-24 22:28:25 -0300

    chore: ajustando o nome da variável de ambiente

--- src/db/postgres/helper.js
+++ src/db/postgres/helper.js
@@ -6,7 +6,7 @@ const { Pool } = pg
 export const pool = new Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  database: process.env.POSTGRES_DB,
   port: process.env.POSTGRES_PORT,
   host: process.env.POSTGRES_HOST,
 })
