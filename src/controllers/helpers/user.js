f844c60d1109b7dffb2c84c659747389b0134034
Deibson Cogo <deibsoncogo@outlook.com>
2024-07-14 20:36:22 -0300

    refactor: remoção de código que não estava sendo utilizado

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,12 +1,4 @@
-import { badRequest, notFound } from "./http.js"
-
-export const invalidPasswordResponse = () => {
-  return badRequest({ message: "Password must be at least 6 characters" })
-}
-
-export const emailIsAlreadyInUseResponse = () => {
-  return badRequest({ message: "Invalid e-mail, please provide a valid one" })
-}
+import { notFound } from "./http.js"
 
 export const userNotFoundResponse = () => {
   return notFound({ message: "User not found" })
