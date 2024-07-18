0aa50eb8ff7344e67b58ac75ea4fd3bc9591bf00
Deibson Cogo <deibsoncogo@outlook.com>
2024-07-18 18:46:38 -0300

    test: criação do teste de integração update user

---
+++ src/useCases/user/update-user.test.js
@@ -0,0 +1,164 @@
+import { faker } from "@faker-js/faker"
+import { UpdateUserUseCase } from "./update-user"
+import { EmailAlreadyInUseError } from "../../errors/user"
+
+describe("Update user use case", () => {
+  const user = {
+    id: faker.string.uuid(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password({ length: 7 }),
+  }
+
+  class UpdateUserRepositoryStub {
+    async execute() {
+      return user
+    }
+  }
+
+  class GetUserByEmailRepositoryStub {
+    async execute() {
+      return null
+    }
+  }
+
+  class PasswordHashAdapterStub {
+    async execute() {
+      return "hashed-password"
+    }
+  }
+
+  const makeSut = () => {
+    const updateUserRepositoryStub = new UpdateUserRepositoryStub()
+    const getUserByEmailRepositoryStub = new GetUserByEmailRepositoryStub()
+    const passwordHashAdapterStub = new PasswordHashAdapterStub()
+
+    const updateUserUseCase = new UpdateUserUseCase(
+      passwordHashAdapterStub,
+      getUserByEmailRepositoryStub,
+      updateUserRepositoryStub,
+    )
+
+    return {
+      updateUserUseCase,
+      passwordHashAdapterStub,
+      getUserByEmailRepositoryStub,
+      updateUserRepositoryStub,
+    }
+  }
+
+  test("Should successfully update user (without email and password)", async () => {
+    const { updateUserUseCase } = makeSut()
+
+    const result = await updateUserUseCase.execute(user.id, {
+      firstName: user.firstName,
+      lastName: user.lastName,
+    })
+
+    expect(result).toBe(user)
+  })
+
+  test("Should successfully update user (with email)", async () => {
+    const { updateUserUseCase, getUserByEmailRepositoryStub } = makeSut()
+
+    const executeSpy = jest.spyOn(getUserByEmailRepositoryStub, "execute")
+
+    const result = await updateUserUseCase.execute(user.id, {
+      email: user.email,
+    })
+
+    expect(result).toBe(user)
+    expect(executeSpy).toHaveBeenCalledWith(user.email)
+  })
+
+  test("Should successfully update user (with password)", async () => {
+    const { updateUserUseCase, passwordHashAdapterStub } = makeSut()
+
+    const executeSpy = jest.spyOn(passwordHashAdapterStub, "execute")
+
+    const result = await updateUserUseCase.execute(user.id, {
+      password: user.password,
+    })
+
+    expect(result).toBe(user)
+    expect(executeSpy).toHaveBeenCalledWith(user.password)
+  })
+
+  test("Should throw EmailAlreadyInUseError if email is already in use", async () => {
+    const { updateUserUseCase, getUserByEmailRepositoryStub } = makeSut()
+
+    jest
+      .spyOn(getUserByEmailRepositoryStub, "execute")
+      .mockResolvedValueOnce(user)
+
+    const result = updateUserUseCase.execute(faker.string.uuid(), {
+      email: user.email,
+    })
+
+    await expect(result).rejects.toThrow(new EmailAlreadyInUseError(user.email))
+  })
+
+  test("Should call UpdateUserRepositoryStub with correct params", async () => {
+    const { updateUserUseCase, updateUserRepositoryStub } = makeSut()
+
+    const executeSpy = jest.spyOn(updateUserRepositoryStub, "execute")
+
+    await updateUserUseCase.execute(user.id, {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      password: user.password,
+    })
+
+    expect(executeSpy).toHaveBeenCalledWith(user.id, {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      password: "hashed-password",
+    })
+  })
+
+  test("Should throw if GetUserByEmailRepositoryStub throws", async () => {
+    const { updateUserUseCase, getUserByEmailRepositoryStub } = makeSut()
+
+    jest
+      .spyOn(getUserByEmailRepositoryStub, "execute")
+      .mockRejectedValueOnce(new Error())
+
+    const result = updateUserUseCase.execute(user.id, {
+      email: user.email,
+    })
+
+    await expect(result).rejects.toThrow()
+  })
+
+  test("Should throw if PasswordHashAdapterStub throws", async () => {
+    const { updateUserUseCase, passwordHashAdapterStub } = makeSut()
+
+    jest
+      .spyOn(passwordHashAdapterStub, "execute")
+      .mockRejectedValueOnce(new Error())
+
+    const result = updateUserUseCase.execute(user.id, {
+      password: user.password,
+    })
+
+    await expect(result).rejects.toThrow()
+  })
+
+  test("Should throw if UpdateUserRepositoryStub throws", async () => {
+    const { updateUserUseCase, updateUserRepositoryStub } = makeSut()
+
+    jest
+      .spyOn(updateUserRepositoryStub, "execute")
+      .mockRejectedValueOnce(new Error())
+
+    const result = updateUserUseCase.execute(user.id, {
+      firstName: user.firstName,
+      lastName: user.lastName,
+    })
+
+    await expect(result).rejects.toThrow()
+  })
+})
