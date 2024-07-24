7fdf22693a173a79213b980bea39e73f75493faa
Deibson Cogo <deibsoncogo@outlook.com>
2024-07-23 22:28:44 -0300

    test: criação do test get user by email repository

---
+++ src/repositories/user/get-user-by-email.test.js
@@ -0,0 +1,27 @@
+import { prisma } from "../../../prisma/prisma.js"
+import { user as userFake } from "../../tests/index.js"
+import { GetUserByEmailRepository } from "./get-user-by-email.js"
+
+describe("Get user by email repository", () => {
+  test("Should get user by email on data base", async () => {
+    const user = await prisma.users.create({ data: userFake })
+
+    const sut = new GetUserByEmailRepository()
+
+    const result = await sut.execute(user.email)
+
+    expect(result).toStrictEqual(user)
+  })
+
+  test("Should call Prisma with correct params", async () => {
+    const sut = new GetUserByEmailRepository()
+
+    const executeSpy = jest.spyOn(prisma.users, "findUnique")
+
+    await sut.execute(userFake.email)
+
+    expect(executeSpy).toHaveBeenCalledWith({
+      where: { email: userFake.email },
+    })
+  })
+})
