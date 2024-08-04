932747e9b36d20273409a33c9f5cfae62aba368c
Deibson Cogo <deibsoncogo@outlook.com>
2024-08-04 12:01:08 -0300

    test: criação dos testes e2e de transactions

---
+++ src/routes/transactions.e2e.test.js
@@ -0,0 +1,93 @@
+import { faker } from "@faker-js/faker"
+import supertest from "supertest"
+import { app } from "../app.js"
+import { transactionPartial, userPartial } from "../tests/index.js"
+
+describe("Transactions router E2E", () => {
+  test("POST /api/transactions should return 201 when transaction is created", async () => {
+    const { body: user } = await supertest(app)
+      .post("/api/users")
+      .send(userPartial)
+
+    const result = await supertest(app)
+      .post("/api/transactions")
+      .send({ ...transactionPartial, userId: user.id })
+
+    expect(result.status).toBe(201)
+  })
+
+  test("GET /api/transactions?userId should return 200 when transaction is found", async () => {
+    const { body: user } = await supertest(app)
+      .post("/api/users")
+      .send(userPartial)
+
+    const { body: transaction } = await supertest(app)
+      .post("/api/transactions")
+      .send({ ...transactionPartial, userId: user.id })
+
+    const result = await supertest(app).get(
+      `/api/transactions?userId=${user.id}`,
+    )
+
+    expect(result.status).toBe(200)
+    expect(result.body[0].id).toBe(transaction.id)
+  })
+
+  test("GET /api/transactions/:userId should return 404 when transaction is not found", async () => {
+    const result = await supertest(app).get(
+      `/api/transactions?userId=${faker.string.uuid()}`,
+    )
+
+    expect(result.status).toBe(404)
+  })
+
+  test("PATCH /api/transactions/:transactionId should return 200 when transaction is updated", async () => {
+    const { body: user } = await supertest(app)
+      .post("/api/users")
+      .send(userPartial)
+
+    const { body: transaction } = await supertest(app)
+      .post("/api/transactions")
+      .send({ ...transactionPartial, userId: user.id })
+
+    const result = await supertest(app)
+      .patch(`/api/transactions/${transaction.id}`)
+      .send({ amount: 100 })
+
+    expect(result.status).toBe(200)
+    expect(result.body.amount).toBe("100")
+  })
+
+  test("PATCH /api/transactions/:transactionId should return 404 when transaction is not found", async () => {
+    const result = await supertest(app)
+      .patch(`/api/transactions/${faker.string.uuid()}`)
+      .send({ amount: 100 })
+
+    expect(result.status).toBe(404)
+  })
+
+  test("DELETE /api/transactions/:transactionId should return 200 when transaction is deleted", async () => {
+    const { body: user } = await supertest(app)
+      .post("/api/users")
+      .send(userPartial)
+
+    const { body: transaction } = await supertest(app)
+      .post("/api/transactions")
+      .send({ ...transactionPartial, userId: user.id })
+
+    const result = await supertest(app).delete(
+      `/api/transactions/${transaction.id}`,
+    )
+
+    expect(result.status).toBe(200)
+    expect(result.body.id).toBe(transaction.id)
+  })
+
+  test("DELETE /api/transactions/:transactionId should return 404 when transaction is not found", async () => {
+    const result = await supertest(app).delete(
+      `/api/transactions/${faker.string.uuid()}`,
+    )
+
+    expect(result.status).toBe(404)
+  })
+})
