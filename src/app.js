233b3ab192e06a8ef34346fd2d775e33cc78a397
Deibson Cogo <deibsoncogo@outlook.com>
2024-08-04 21:18:23 -0300

    chore: criação das configurações do swagger

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import express from "express"
+import fs from "node:fs"
+import path from "node:path"
+import swagger from "swagger-ui-express"
 import { transactionsRouter, usersRouter } from "./routes/index.js"
 
 export const app = express()
@@ -7,3 +10,12 @@ app.use(express.json())
 
 app.use("/api/users", usersRouter)
 app.use("/api/transactions", transactionsRouter)
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(
+    path.join(import.meta.dirname, "../docs/swagger.json"),
+    "utf8",
+  ),
+)
+
+app.use("/docs", swagger.serve, swagger.setup(swaggerDocument))
